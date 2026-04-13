3c45cb352a89fccb4e7cf2cf395b53e68133e7af
Clément.A <125081819+ITClmt@users.noreply.github.com>
2026-04-13 23:50:15 +0200

    feat: implement score controller endpoint and add Bruno API test collections

--- src/score/score.controller.ts
+++ src/score/score.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get, Param } from "@nestjs/common";
+import { ScoreService } from "./score.service";
 
 @Controller("score")
-export class ScoreController {}
+export class ScoreController {
+  constructor(private readonly scoreService: ScoreService) {}
+
+  @Get("user/:id")
+  async getUserScore(@Param() params: { id: string }) {
+    return this.scoreService.getUserScores(params.id);
+  }
+}
