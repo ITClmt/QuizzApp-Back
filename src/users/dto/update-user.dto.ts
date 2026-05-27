19114201411ff12305b44ac4efbd23e2ea9c294d
Clément.A <125081819+ITClmt@users.noreply.github.com>
2026-05-27 15:03:34 +0200

    fix: remove password update from user PATCH endpoint

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,4 +1,17 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateUserDto } from './create-user.dto';
+import { IsEmail, IsOptional, IsString, MaxLength, MinLength } from "class-validator";
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto {
+  @IsOptional()
+  @IsEmail()
+  email?: string;
+
+  @IsOptional()
+  @IsString()
+  @MinLength(3, { message: "Name must be at least 3 characters long" })
+  @MaxLength(20, { message: "Name must be at most 20 characters long" })
+  username?: string;
+
+  @IsOptional()
+  @IsString()
+  lang?: string;
+}
